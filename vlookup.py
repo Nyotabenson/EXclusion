79a11651dd81c145a71691042ea115eecfacfd33
Benson <nyota.benson752@gmail.com>
2024-02-15 18:08:45 +0300

    try and exception

--- vlookup.py
+++ vlookup.py
@@ -30,32 +30,37 @@ if sheet is not None:
 
 df = st.file_uploader("Choose a file to upload:", type=["xlsx"])
 if df is not None:
-    df1 = pd.read_excel(df, sheet_name=sheet, header=None)
-    st.write(df1)
+
+    try:
+
+        df1 = pd.read_excel(df, sheet_name=sheet, header=None)
+        st.write(df1)
 
 
 
-    #Operation
-    # Select rows where value in column1 is not in column2
-    filtered_df = df1[~df1[0].isin(df1[1])]
+        #Operation
+        # Select rows where value in column1 is not in column2
+        filtered_df = df1[~df1[0].isin(df1[1])]
 
 
-    filtered_df = filtered_df.drop(filtered_df.columns[1:], axis=1)
+        filtered_df = filtered_df.drop(filtered_df.columns[1:], axis=1)
 
-    # Print the result
-    st.write(len(filtered_df), "Uniques Entries")    
+        # Print the result
+        st.write(len(filtered_df), "Uniques Entries")    
 
-    st.write(filtered_df)
-
-
-
-    csv_string = filtered_df.to_csv(index=False, header=None)
-    csv_bytes = csv_string.encode()
+        st.write(filtered_df)
 
 
-    st.download_button(
-        label="Download data",
-        data=csv_bytes,
-        file_name="data.csv",
-        mime="text/csv"
-    )
+
+        csv_string = filtered_df.to_csv(index=False, header=None)
+        csv_bytes = csv_string.encode()
+
+
+        st.download_button(
+            label="Download data",
+            data=csv_bytes,
+            file_name="data.csv",
+            mime="text/csv"
+        )
+    except:
+        st.error("Kindly follow the Instructions:")
