162fe28d6d293d1cf7a4d1ca5e90f1c2c7e9f27c
Benson <nyota.benson752@gmail.com>
2024-02-15 17:59:03 +0300

    Header

--- vlookup.py
+++ vlookup.py
@@ -3,7 +3,24 @@ import pandas as pd
 from io import BytesIO
 
 
-st.title("Exclusion")
+#st.title("Exclusion")
+st.image("HEADER.png", caption=".", use_column_width=True)
+st.subheader("Exclusion")
+st.write("""Exclusion is an advanced program designed to streamline data management by effectively filtering out redundant entries
+          from one column based on the information present in another column. By employing this tool, users can obtain a comprehensive and
+            reliable overview of the unique values inherent in their dataset.
+
+The functionality of Exclusion primarily revolves around a dual-column approach, wherein it meticulously identifies 
+entries in column B that also exist in column A. What sets this program apart is its ability to automatically eliminate 
+these redundant values, thereby leaving users with a refined set of unique data points. This streamlined dataset often contains
+ valuable insights into previously overlooked or unattended values,
+ enhancing the depth and accuracy of data analysis.""")
+
+st.write("---")
+st.subheader("Exclusion Program")
+st.write("##")
+st.write("- Utilize a dataset formatted in Excel with the extension '.xlsx'.")
+st.write("- The two features should be side by side.")
 
 sheet = st.text_input("Type the sheet name:")
 if sheet is not None:
@@ -26,7 +43,7 @@ if df is not None:
     filtered_df = filtered_df.drop(filtered_df.columns[1:], axis=1)
 
     # Print the result
-    st.write(len(filtered_df), "Entries")    
+    st.write(len(filtered_df), "Uniques Entries")    
 
     st.write(filtered_df)
 
